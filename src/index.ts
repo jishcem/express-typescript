add36a6c0632a67fe0144318d05ce05e762c572e
Ajeesh Joshy <jishcem@gmail.com>
2022-06-19 08:45:36 +0530

    First commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World from Express+TypeScript");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);    
+});
